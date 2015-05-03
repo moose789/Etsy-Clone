474c2272429b4ec914d3488ab2a39405f491aadc
moose789 <hanahb23@gmail.com>
2015-05-03 19:30:43 +0000

    added figaro gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.dropbox_app_key = "wmlj7xrai0st6r1"
   config.dropbox_app_secret = ENV["APP_SECRET"]
   config.dropbox_access_token = "79fzl7vm5mjppsf1"
-  config.dropbox_access_token_secret = "ENV["ACCESS_TOKEN_SECRET"]"
+  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
   config.dropbox_user_id = "417653578"
   config.dropbox_access_type = "app_folder"
 end

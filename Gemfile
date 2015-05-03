dbff525186d54bf7a29fcda047e56109253c5d11
moose789 <hanahb23@gmail.com>
2015-05-03 18:42:59 +0000

    Added Dropbox folder for Images

--- Gemfile
+++ Gemfile
@@ -19,6 +19,11 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
+
+
+gem 'carrierwave'
+gem 'carrierwave-dropbox'
+
 group :production do
   gem 'pg'
   gem 'rails_12factor'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327033839) do
+ActiveRecord::Schema.define(version: 20150503174921) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150327033839) do
     t.string   "decimal"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
